287142249f78e96c84ca5fe4943253cb49c7aa48
kxmorocho <kxmorocho@utpl.edu.ec>
2018-10-12 10:21:49 -0500

    crear clase precio y principal

---
+++ Clase2/src/clase2/Clase2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase2;
+
+/**
+ *
+ * @author TOSHIBA
+ */
+public class Clase2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Precio p;
+        p = new Precio();
+        System.out.println(p.euros);
+    }
+
+}
---
+++ Clase2/src/clase2/Precio.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase2;
+
+/**
+ *
+ * @author TOSHIBA
+ */
+public class Precio {
+
+    double euros;
+
+    public double obtener() {
+        return 0;
+    }
+
+    public double agregar(double x) {
+        return 0;
+    }
+}
